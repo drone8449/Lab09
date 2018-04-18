311fa74d8cc58c4c9d0eb502b38e43d1204fb294
leny <leny@172.17.192.90>
2018-04-18 23:06:14 +0900

    array_to_arraylist

--- src/main/java/edu/handong/csee/java/lab09/MarketingAnalyzer.java
+++ src/main/java/edu/handong/csee/java/lab09/MarketingAnalyzer.java
@@ -1,6 +1,7 @@
 package edu.handong.csee.java.lab09; // package name
 
 import java.util.Scanner; // import Scanner Class
+import java.util.ArrayList;
 
 /**
  * This program is made for analyzing marketing sales of employee
@@ -34,8 +35,7 @@ public class MarketingAnalyzer { // make public class 'MarketingAnalyzer'
 	/**
 	 * This is method for setting name
 	 * 
-	 * @param n
-	 *            is name
+	 * @param n is name
 	 */
 	public void setName(String n) { // make public method 'setName', and parameter is n
 		name = n; // name is n
@@ -44,8 +44,7 @@ public class MarketingAnalyzer { // make public class 'MarketingAnalyzer'
 	/**
 	 * This is method for setting sales
 	 * 
-	 * @param s
-	 *            is sales
+	 * @param s is sales
 	 */
 	public void setSales(double s) { // make public method 'setSales', and parameter is s
 		sales = s; // sales is s
@@ -53,7 +52,7 @@ public class MarketingAnalyzer { // make public class 'MarketingAnalyzer'
 
 	private double highestSales; // declare double highestSales
 	private double averageSales; // declare double averageSales
-	private MarketingAnalyzer[] team; // declare Array team
+	private ArrayList<MarketingAnalyzer> team; // declare Array team
 
 	/**
 	 * main method
@@ -61,8 +60,7 @@ public class MarketingAnalyzer { // make public class 'MarketingAnalyzer'
 	 * @param args
 	 */
 	public static void main(String[] args) { // main method
-		MarketingAnalyzer analyzer = new MarketingAnalyzer(); // declare object 'analyzer' to use MarketingAnalyzer
-																// Class
+		MarketingAnalyzer analyzer = new MarketingAnalyzer(); // declare object 'analyzer' to use MarketingAnalyzer Class
 
 		analyzer.getData(); // call getData()
 		analyzer.computeAverage(); // call computeAverage()
@@ -74,24 +72,27 @@ public class MarketingAnalyzer { // make public class 'MarketingAnalyzer'
 	 * This is method for getting data
 	 */
 	public void getData() { // make public method 'getData'
-		System.out.println("Enter the number of salesmen: "); // print message 'Enter the number of salesmen: '
+		System.out.println("Enter the name and sales of salesmen: "); // print message 'Enter the number of salesmen: '
 		Scanner mykeyboard = new Scanner(System.in); // declare object 'mykeyboard' to use Scanner Class
 
-		int numOfSalesmen = mykeyboard.nextInt(); // input value is numOfSalesmen
+		team = new ArrayList<MarketingAnalyzer>();
 
-		team = new MarketingAnalyzer[numOfSalesmen]; // make array object 'team'
-
-		for (int i = 0; i < numOfSalesmen; i++) { // repeat i times from 0 to numOfSalesmen
-			System.out.println("Enter data for associate number: " + (i + 1)); // print message 'Enter data for
-																				// associate number: ' and
+	    boolean done = false; 
+	    
+		while (!done) { 
 			System.out.print("Name? "); // print message 'Name? '
-			mykeyboard.nextLine(); // get 'enter' value
 			String name = mykeyboard.nextLine(); // input value is name
 			System.out.print("Sales? "); // print message 'Sales? '
 			double sales = mykeyboard.nextDouble(); // input value is sales
-			team[i] = new MarketingAnalyzer(); // make object 'MarketingAnalyzer'
-			team[i].setName(name); // set the name
-			team[i].setSales(sales); // set the Sales
+			MarketingAnalyzer t = new MarketingAnalyzer();
+			t.setName(name);
+			t.setSales(sales);
+			team.add(t);
+			System.out.print("Do you want to get additional data?");
+			mykeyboard.nextLine();
+			String ans = mykeyboard.nextLine();
+			if(!ans.equalsIgnoreCase ("yes"))
+				done = true;
 		}
 	}
 
@@ -100,12 +101,12 @@ public class MarketingAnalyzer { // make public class 'MarketingAnalyzer'
 	 */
 	public void computeAverage() { // make public method 'computeAverage'
 		double sum = 0; // declare double sum
-
-		for (int i = 0; i < team.length; i++) { // repeat i times from 0 to team.length
-			sum = sum + team[i].getSales(); // get everyone's sales
+     
+		for (int i = 0; i < team.size(); i++) { // repeat i times from 0 to team.length
+			sum = sum + team.get(i).getSales(); // get everyone's sales
 		}
 
-		averageSales = sum / team.length; // get average sales
+		averageSales = sum / team.size(); // get average sales
 	}
 
 	/**
@@ -114,9 +115,9 @@ public class MarketingAnalyzer { // make public class 'MarketingAnalyzer'
 	public void computeHighestSales() { // make public method 'computeHighestSales'
 		double highestSales = 0; // declare double highestSales
 
-		for (int i = 0; i < team.length; i++) { // repeat i times from 0 to team.length
-			if (highestSales < team[i].getSales()) // if team[i].getSales is bigger than anything else
-				highestSales = team[i].getSales(); // get highestSales
+		for (int i = 0; i < team.size(); i++) { // repeat i times from 0 to team.length
+			if (highestSales < team.get(i).getSales()) // if team[i].getSales is bigger than anything else
+				highestSales = team.get(i).getSales(); // get highestSales
 		}
 
 		this.highestSales = highestSales; // highestSales is Class MarketingAnalyzer's variable 'highestSales'
@@ -131,10 +132,10 @@ public class MarketingAnalyzer { // make public class 'MarketingAnalyzer'
 		System.out.println("Salesmen who has the highest sales: "); // print message "Salesmen who has the highest
 																	// sales: "
 
-		for (int i = 0; i < team.length; i++) { // repeat i times from 0 to team.length
-			if (team[i].getSales() == highestSales) { // if get highestSales team
-				System.out.println("Name: " + team[i].getName()); // print highestSales team name
-				System.out.println("Sales: " + team[i].getSales()); // print highestSales team's sales
+		for (int i = 0; i < team.size(); i++) { // repeat i times from 0 to team.length
+			if (team.get(i).getSales() == highestSales) { // if get highestSales team
+				System.out.println("Name: " + team.get(i).getName()); // print highestSales team name
+				System.out.println("Sales: " + team.get(i).getSales()); // print highestSales team's sales
 			}
 		}
 	}
